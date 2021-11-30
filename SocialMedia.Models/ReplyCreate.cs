00ac5e3e6b5c580b1dc13d05702b3bd433903bbe
Rex Meyer <armeyer1998@gmail.com>
2021-11-30 18:50:08 -0500

    reply class added

---
+++ SocialMedia.Models/ReplyCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class ReplyCreate
+    {
+        public int ReplyID { get; set; }
+        [MaxLength(200, ErrorMessage = "Comment is too long (Max 200).")]
+        public string Text { get; set; }
+        //public int CommentID { get; set; }
+        //public virtual Comment Comment { get; set; }
+    }
+}
