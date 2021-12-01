569a4114b1f4c64b08526ea8c1aa34ac1b9102cf
Nathan Foreman <nathanforeman746@gmail.com>
2021-12-01 16:54:00 -0600

    post page added

---
+++ SocialMedia.Data/PostComments.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Data
+{
+    public class PostComments
+    {
+        [Key]
+        public int PostId { get; set; }
+
+        [Required]
+        public Guid AuthorId { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public virtual ICollection<Post> ListOfPosts { get; set; }
+
+        public PostComments()
+        {
+            ListOfPosts = new HashSet<Post>();
+        }
+    }
+}
---
+++ SocialMedia.Services/PostService.cs
@@ -0,0 +1,106 @@
+﻿using SocialMedia.Data;
+using SocialMedia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Services
+{
+    public class PostService
+    {
+        private readonly Guid _authorId;
+        public PostService(Guid authorId)
+        {
+            _authorId = authorId;
+        }
+        public bool CreatePost(PostCreate model)
+        {
+            var entity =
+                new Post()
+                {
+                    AuthorId = _authorId,
+                    Title = model.Title,
+                    Text = model.Text,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Posts.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<PostListItem> GetPosts()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Posts
+                        .Where(e => e.AuthorId == _authorId)
+                        .Select(
+                            e =>
+                                new PostListItem
+                                {
+                                    PostId = e.PostId,
+                                    Title = e.Title,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                        );
+                return query.ToArray();
+            }
+        }
+        public PostDetail GetPostById(int authorId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Posts
+                    .Single(e => e.PostId == authorId && e.AuthorId == _authorId);
+                return
+                    new PostDetail
+                    {
+                        PostId = entity.PostId,
+                        Title = entity.Title,
+                        Text = entity.Text,
+                        CreatedUtc = entity.CreatedUtc,
+                        ModifiedUtc = entity.ModifiedUtc
+                    };
+            }
+        }
+        public bool UpdatePost(PostEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Posts
+                    .Single(e => e.PostId == model.PostId && e.AuthorId == _authorId);
+
+                    entity.Title = model.Title;
+                    entity.Text = model.Text;
+                    entity.ModifiedUtc = DateTimeOffset.UtcNow;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public bool DeletePost(int postId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Posts
+                        .Single(e => e.PostId == postId && e.AuthorId == _authorId);
+
+                    ctx.Posts.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
+
