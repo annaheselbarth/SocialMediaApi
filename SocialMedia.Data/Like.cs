e1e99faa7e6569a4086a13fa7e54c308b81f8413
Anna Heselbarth <annaheselbarth@gmail.com>
2021-12-01 11:54:12 -0500

    added like classes

---
+++ SocialMedia.Data/Like.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Data
+{
+    public class Like
+    {
+        [Key]
+        public int PostId { get; set; }
+        [ForeignKey("Owner")]
+        public Guid OwnerId { get; set; }
+        public int LikeId { get; set; }
+        public virtual ApplicationUser Owner { get; set; }
+    }
+}
---
+++ SocialMedia.WebApi/Controllers/LikeController.cs
@@ -0,0 +1,74 @@
+﻿using SocialMedia.Data;
+using SocialMedia.Models;
+using SocialMedia.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SocialMedia.WebApi.Controllers
+{
+    public class LikeController : ApiController
+    {
+
+        private Like CreateLike()
+        {
+            var ownerId = Guid.Parse(User.Identity.GetOwnerId());
+            var like = new Like(ownerId);
+            return like;
+        }
+
+        public IHttpActionResult Get()
+        {
+            LikeService likeService = CreateLikeService();
+            var likes = likeService.GetLikes();
+            return Ok(likes);
+        }
+        public IHttpActionResult Post(LikeCreate like)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateLike();
+
+            if (!service.CreateLike(like))
+                return InternalServerError();
+
+            return Ok();
+
+
+        }
+
+        private LikeService CreateLikeService()
+        {
+            var userId = Guid.Parse(User.Identity.GetOwnerId());
+            var LikeService = new LikeService(userId);
+            return LikeService;
+        }
+
+        public IHttpActionResult Put(LikeEdit like)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateLikeService();
+
+            if (!service.UpdateLike(like))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int likeId)
+        {
+            var service = CreateLikeService();
+
+            if (!service.DeleteLike(likeId))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
